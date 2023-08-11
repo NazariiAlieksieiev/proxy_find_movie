9ab5c1861b1e329b0e630cff97976beac422d7d1
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-08-11 16:25:33 +0300

    Change openAi API config

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,35 @@
-const openAI = require('openai')
+const { Configuration, OpenAIApi } = require("openai");
 const express = require('express');
 const router = express.Router();
-const needle = require('needle');
-
 const API_KEY = process.env.API_KEY;
-const API_REQUEST = process.env.OPENAI_REQ
+
+const config = new Configuration({
+  apiKey: API_KEY,
+});
+
+const openai = new OpenAIApi(config);
 
 router.post('/', async (req, res) => {
   const { sendedMessages } = req.body;
   console.log(sendedMessages)
   try {
-    const headers = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${API_KEY}`,
-    };
-    
-    const data = {
+    const completion = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
       messages: [
-        { role: 'system', 
-        content: 'Ти сервіс з пошуку фільмів, серіалі, мультиків, мультсеріалів'},
+        {
+          role: 'system',
+          content: 'Ти сервіс з пошуку фільмів, серіалі, мультиків, мультсеріалів'
+        },
         ...sendedMessages
       ]
-    };
-    
-    const respApi = await needle('post', API_REQUEST, data, { headers });
-    const message = respApi.body.choices[0].message
+    }
+    )
+
+    const message = completion.data.choices[0].message
 
     res.status(200).json(message);
   } catch (error) {
-    res.status(500).json({error})
+    res.status(500).json({ error })
   }
 })
 
